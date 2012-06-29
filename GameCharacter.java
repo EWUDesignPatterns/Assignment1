9ebee6fb1e8420f27600e15170d03aa99369e0e9
Michael Williams <Mike@micahel-williams.local>
2012-06-28 17:12:26 -0700

    Add GuitarHero class for main class and add GameCharacter class for implementation of game characters.

---
+++ GameCharacter.java
@@ -0,0 +1,14 @@
+public class GameCharacter {
+	private GuitarBehavior guitar;
+	private SoloBehavior solo;
+	
+	public void	playGuitar()
+	{
+		
+	}
+	
+	public void playSolo()
+	{
+		
+	}
+}
---
+++ GuitarHero.java
@@ -0,0 +1,11 @@
+public class GuitarHero {
+	public static void main(String[] args) {
+		
+		GameCharacter player1 = new GameCharacterSlash();
+        GameCharacter player2 = new GameCharacterHendrix();
+        player1.playGuitar();
+        player2.playGuitar();
+        player1.playSolo();
+        player2.playSolo();
+	}
+}
