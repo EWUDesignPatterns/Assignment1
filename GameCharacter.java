b1033c7b8aa728ccbf1aad117989a60df54c5c55
Michael Williams <Mike@micahel-williams.local>
2012-06-28 17:19:23 -0700

    - Make GameCharacters have a name

--- GameCharacter.java
+++ GameCharacter.java
@@ -2,6 +2,17 @@ public class GameCharacter {
 	private GuitarBehavior guitar;
 	private SoloBehavior solo;
 	
+	private name;
+	
+	public void GameCharacter(String name) {
+	  this.name = name;
+	}
+	
+	public String getName()
+	{
+	  return this.name;
+	}
+
 	public void	playGuitar()
 	{
 		this.guitar.playGuitar();
