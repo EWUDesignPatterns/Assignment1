f598f6d68504d8139cf62a391241c4a824236797
Jon Roster <jroster76@gmail.com>
2012-06-28 17:45:08 -0700

    added missing brace

--- Jump.java
+++ Jump.java
@@ -1,4 +1,7 @@
-class Jump implements SoloBehavior {
-	public void playSolo(String name) {
+class Jump implements SoloBehavior 
+{
+	public void playSolo(String name)
+	{
 		System.out.println(name + " has just jumped off the stage. Wow!");
+	}
 }
